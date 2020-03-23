a7a3e1e1d4bd2005d6c1ed0f9bf74ccf00d53ded
Mohammad-Quanit <muhammadquanit@gmail.com>
2020-03-23 15:54:55 +0500

    some changes...

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is an automated react app
+          This is an automated react app hosted on AWS S3
         </p>
         <a
           className="App-link"
